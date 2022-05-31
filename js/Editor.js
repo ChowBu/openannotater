f866c98e24e3d2ee99b1b8868c0f114e0adafa92
ChowBu <675071914@qq.com>
2022-05-31 13:00:33 +0800

    update camera up

--- js/Editor.js
+++ js/Editor.js
@@ -10,6 +10,7 @@ var _DEFAULT_CAMERA = new THREE.PerspectiveCamera( 50, 1, 0.01, 1000 );
 _DEFAULT_CAMERA.name = 'Camera';
 _DEFAULT_CAMERA.position.set( 0, 5, 10 );
 _DEFAULT_CAMERA.lookAt( new THREE.Vector3() );
+_DEFAULT_CAMERA.up.copy( new THREE.Vector3(0, 0, 1));
 
 function Editor() {
 
