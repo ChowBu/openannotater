5211de94b4c8e920b144d1adb6594860b1607f42
ChowBu <675071914@qq.com>
2022-06-08 00:04:47 +0800

    feat: update add ui

--- js/Menubar.js
+++ js/Menubar.js
@@ -1,6 +1,6 @@
 import { UIPanel } from './libs/ui.js';
 
-// import { MenubarAdd } from './Menubar.Add.js';
+import { MenubarAdd } from './Menubar.Add.js';
 import { MenubarEdit } from './Menubar.Edit.js';
 import { MenubarFile } from './Menubar.File.js';
 // import { MenubarExamples } from './Menubar.Examples.js';
@@ -14,9 +14,9 @@ function Menubar( editor ) {
 	const container = new UIPanel();
 	container.setId( 'menubar' );
 
+	container.add( new MenubarAdd( editor ) );
 	container.add( new MenubarFile( editor ) );
 	container.add( new MenubarEdit( editor ) );
-	// container.add( new MenubarAdd( editor ) );
 	// container.add( new MenubarPlay( editor ) );
 	// container.add( new MenubarExamples( editor ) );
 	container.add( new MenubarView( editor ) );
